9c197e8d4b22611ab50ab0c4f50c448d2a1829f5
Tom McGuire <atomicwire@gmail.com>
2022-04-08 15:01:32 -0500

    Modified for production and added twinkle

--- OutfieldControllerCode.ino
+++ OutfieldControllerCode.ino
@@ -25,21 +25,39 @@
       return to step 2. */
 
 const char Description;
-#define DEBUG 1
-const uint8_t ButtonInput[] ={4,6,8,10,12,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54};
-const uint8_t ButtonOutput[] ={3,5,7,9,11,13,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55};
-uint8_t ButtonInPlay[25] ={0};
-uint8_t OutputInPlay[25] = {0};
+//#define DEBUG 1
+const uint8_t ButtonInput[] ={24,26,28,30,32,34,36,38,40,42,44,46,48,50,52};
+const uint8_t ButtonOutput[] ={25,27,29,31,33,35,37,39,41,43,45,47,49,51,53};
+uint8_t ButtonInPlay[15] ={0};
+uint8_t OutputInPlay[15] = {0};
 int NumOfButtons = 0;
 unsigned long startMillis = 0;   
 unsigned long playTime = 0;   
 char playMode = 'A';    
-int StartInput = 2;
-int StartOutput = 13;
-int StartLedState = 0;
-int outfieldButtonFlashRate = 500; //  in milliseconds
+int StartInput = 22;
+int StartOutput = 23;
+bool StartLedState = 1;
+int StartLedBlinkCount = 0;
+int outfieldButtonFlashRate = 90; //  in milliseconds
+int introButtonFlashRate = 300;
 int randomButton = 0;
+int randSelect;
 int wrongBall = -1;
+int sparkleArray[20]={0};
+int flashCount;
+
+int ScanTheOutfield(void){  
+  //look for all button inputs that are being pulled high (button connected)
+  int ButtonCount = 0;
+  for(int i = 0;i <= sizeof(ButtonInput);i++){
+    if(digitalRead(ButtonInput[i])==HIGH){
+      ButtonInPlay[ButtonCount] = ButtonInput[i];
+      OutputInPlay[ButtonCount] = ButtonOutput[i];
+      ++ButtonCount;
+    }
+  }
+  return ButtonCount;
+}
 
 void setup() {
 pinMode(StartOutput, OUTPUT);
@@ -57,7 +75,7 @@ pinMode(StartInput, INPUT_PULLUP);
 pinMode(StartOutput, OUTPUT);
   // Initailize the array of pins considered to be inputs in order
 for(int i=0;i < sizeof(ButtonInput);i++){
-  pinMode(ButtonInput[i],INPUT_PULLUP);
+  pinMode(ButtonInput[i],INPUT);
   #ifdef DEBUG
     Serial.print(i,DEC);
     Serial.print(" ");
@@ -69,31 +87,44 @@ for(int i=0;i <= sizeof(ButtonOutput);i++){
   pinMode(ButtonOutput[i],OUTPUT);
   digitalWrite(ButtonOutput[i],LOW);
   }
+NumOfButtons = ScanTheOutfield();    //Look to see how many buttons there are
 }
 
-int ScanTheOutfield(void){  
-  //look for all button inputs that are being pulled high (button connected)
-  int ButtonCount = 0;
-  for(int i = 0;i <= sizeof(ButtonInput);i++){
-    if(digitalRead(ButtonInput[i])==HIGH){
-      ButtonInPlay[ButtonCount] = ButtonInput[i];
-      OutputInPlay[ButtonCount] = ButtonOutput[i];
-      ++ButtonCount;
-    }
-  }
-  return ButtonCount;
-}
 
 void loop() {
   // int i = 0;
   Serial3.println("A");
-  while(playMode == 'A'){  // Blink the Start button slowly and wait for the button to be pushed
-    for(int i = 0;i<1000;i++){
-      delay(2);
-      if(i == 500)digitalWrite(StartOutput,HIGH);
+  while(playMode == 'A'){  // Blink or turn on the Start button and wait for the button to be pushed
+//    for(int i = 0;i < flashCount; i++){
+  digitalWrite(StartOutput,StartLedState);
+  ++StartLedBlinkCount;
+  if(StartLedBlinkCount > 8){  //the flash rate of the start button
+    StartLedState = !(StartLedState);
+    StartLedBlinkCount = 0;
+  }
+      randSelect = random(15); //larger num = fewer, smaller num = more ball flashes
+      if(randSelect < NumOfButtons){
+        sparkleArray[randSelect] = 20;  // the duration of a light
+        randomButton = randSelect;
+      }
+      for(int j = 0;j < NumOfButtons;j++){
+        if(sparkleArray[j]){
+          digitalWrite(OutputInPlay[j],HIGH);
+          sparkleArray[j]--;
+        }else{digitalWrite(OutputInPlay[j],LOW);}
+      }
+      delay(introButtonFlashRate);
+//    }
+    
+//    for(int i = 0;i<1000;i++){
+//      delay(2);
+//      if(i == 500)digitalWrite(StartOutput,HIGH);
       if(digitalRead(StartInput)==LOW){   // Look to see if someone has mashed the StartButton
         digitalWrite(StartOutput,LOW);
-        NumOfButtons = ScanTheOutfield();    //Look to see how many buttons there are
+        for(int j = 0;j < NumOfButtons;j++){ // clear the field
+          digitalWrite(OutputInPlay[j],LOW); 
+        }
+        NumOfButtons = ScanTheOutfield();    //Look again to see how many buttons there are
         #ifdef DEBUG
         Serial.println(NumOfButtons,DEC);  //if DEBUG print all the active Buttons and Outputs
         for(int i = 0;i < NumOfButtons;i++){
@@ -105,28 +136,48 @@ void loop() {
           Serial.print(OutputInPlay[i],DEC);
           Serial.print(" ");
         } 
-        Serial.println("");
+        Serial.println("  B");
         #endif
        playMode = 'B';
         Serial3.println("B");
-        i = 1000;   // break from the for loop
+//        i = 1000;   // break from the for loop
       }
-    } 
+//    } 
     digitalWrite(StartOutput,LOW);  // make sure the start button light is out
   }
   randomSeed(millis()); // Start making up random numbers
   while(playMode == 'B'){  //randomly flash the buttons in play a random number of times
-    int flashCount = random(15,25);
-    randomButton = 0;
-    for(int i = 0;i < flashCount;i++){
-      randomButton = random(0,NumOfButtons);
-      digitalWrite(OutputInPlay[randomButton],HIGH);
+    flashCount = random(90,150);
+    for(int i = 0;i < flashCount; i++){
+      randSelect = random(30); //larger num = fewer, smaller num = more ball flashes
+      if(randSelect < NumOfButtons){
+        sparkleArray[randSelect] = 3;  // the duration of a light
+        randomButton = randSelect;
+      }
+      for(int j = 0;j < NumOfButtons;j++){
+        if(sparkleArray[j]){
+          digitalWrite(OutputInPlay[j],HIGH);
+          sparkleArray[j]--;
+        }else{
+          digitalWrite(OutputInPlay[j],LOW);
+        }
+      }
       delay(outfieldButtonFlashRate);
-      #ifdef DEBUG
-        Serial.print(OutputInPlay[randomButton],DEC);Serial.print(" ");
-      #endif
-      digitalWrite(OutputInPlay[randomButton],LOW);
     }
+    for(int j = 0;j < NumOfButtons;j++){
+      digitalWrite(OutputInPlay[j],LOW); 
+    }
+//    int flashCount = random(15,25);
+//    randomButton = 0;
+//    for(int i = 0;i < flashCount;i++){
+//      randomButton = random(0,NumOfButtons);
+//      digitalWrite(OutputInPlay[randomButton],HIGH);
+//      delay(outfieldButtonFlashRate);
+//      #ifdef DEBUG
+//        Serial.print(OutputInPlay[randomButton],DEC);Serial.print(" ");
+//      #endif
+//      digitalWrite(OutputInPlay[randomButton],LOW);
+//    }
     playMode = 'C';
   }
     Serial3.println("C");
@@ -146,6 +197,11 @@ void loop() {
       Serial3.print("D");
       Serial3.println(playTime,DEC);   // Send the score time
       playMode = 'D';
+      #ifdef DEBUG
+        Serial.print("D ");
+        Serial.println(playTime,DEC);
+        randomButton = 1;
+      #endif
     }
     for(int i = 0;i < NumOfButtons;i++){ // if the wrong button is pushed continue
       if(i != randomButton){
@@ -160,9 +216,9 @@ void loop() {
         }
       }
     } 
-    if(millis() > (startMillis + 9990)){    // if timeout send a Y, then start over....
+    if(millis() > (startMillis + 9900)){    // if timeout send a T, then start over....
       Serial3.println("T");    
-       playTime = (millis() - startMillis);
+       playTime = 9999;  // was (millis() - startMillis);
       Serial3.println(playTime,DEC);   // Send the score time
       playMode = 'D'; 
       Serial3.print("D");
